662de234a2f72ea5a64a934ecc015f245faa5f39
Shubham Chhatbar <shubhamchhatbar1@gmail.com>
2025-05-29 22:21:20 +0530

    repl

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <string>
-
+using namespace std;
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
   std::cout << "$ ";
 
   std::string input;
-  std::getline(std::cin, input);
-  std::cout << input << ": command not found" << std::endl;
+  while (true) {
+    cout << "$ ";
+    if(!std::getline(std::cin, input)) break;
+    cout << input << ": command not found" << endl;
+  }
 }
